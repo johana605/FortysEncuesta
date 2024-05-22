f2f947464dc09f9df69a99b11690c3b8fd26a181
Mildred-Sosa <johana605@gmail.com>
2024-05-21 19:39:22 -0500

    Actualizacion

--- config.py
+++ config.py
@@ -2,7 +2,8 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///survey.db')
+    #SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///survey.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.getenv('SECRET_KEY', 'your_secret_key')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'mssql+pyodbc://appMil:Dannie54!@serverenc.database.windows.net:1433/clientesenc?driver=ODBC+Driver+17+for+SQL+Server'
 
